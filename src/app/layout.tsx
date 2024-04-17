c6eff176eb7aa242faf9c0f90816ff9683495172
Yohan <yohan.brl@gmail.com>
2024-04-17 12:33:10 +0200

    feat: add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import NavBar from '@components/_commons/NavBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={`${inter.className} mx-auto max-w-screen-md pb-2`}>
+        <NavBar />
         {children}
       </body>
     </html>
---
+++ src/components/_commons/NavBar.tsx
@@ -0,0 +1,73 @@
+'use client';
+import React from 'react';
+import SettingsIcon from '@assets/SettingsWheelIcon.svg';
+import GeoQuizLogoExpanded from '@assets/LogoExpanded.svg';
+import GeoQuizLogoCompact from '@assets/LogoCompact.svg';
+import HeaderHeroSeparator from '@assets/HeaderHeroSeparator.svg';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+
+const ExpandedHeader = () => (
+  <div className='grid h-full grid-cols-5 grid-rows-1 gap-0 p-4'>
+    <div className='col-span-3 col-start-2'>
+      <Link href='/'>
+        <Image
+          src={GeoQuizLogoExpanded}
+          alt='GeoQuiz Logo'
+          className='mx-auto w-full'
+        />
+      </Link>
+    </div>
+    <div className='col-start-5 flex items-start justify-end'>
+      <button type='button' className='focus:outline-none'>
+        <Image src={SettingsIcon} alt='Settings Icon' className='w-8 md:w-12' />
+      </button>
+    </div>
+  </div>
+);
+const CompactHeader = () => (
+  <div className='flex items-start justify-between px-4 pb-2 pt-3'>
+    <Link href='/'>
+      <Image
+        src={GeoQuizLogoCompact}
+        alt='GeoQuiz Logo'
+        className='w-16 md:w-20'
+      />
+    </Link>
+    <button type='button' className='focus:outline-none'>
+      <Image src={SettingsIcon} alt='Settings Icon' className='w-6 md:w-10' />
+    </button>
+  </div>
+);
+
+function NavBar() {
+  const currentPath = usePathname();
+  const isExpanded = currentPath === '/';
+
+  if (isExpanded) {
+    return (
+      <header>
+        <ExpandedHeader />
+        <Image
+          src={HeaderHeroSeparator}
+          alt='Separator Hero Line'
+          className='w-2/3 pb-4 md:pt-10'
+        />
+      </header>
+    );
+  }
+
+  return (
+    <header>
+      <CompactHeader />
+      <Image
+        src={HeaderHeroSeparator}
+        alt='Separator Hero Line'
+        className='w-2/3 pb-4 pt-0'
+      />
+    </header>
+  );
+}
+
+export default NavBar;
