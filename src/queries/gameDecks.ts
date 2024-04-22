a4a93d26c79992e8bf4eceb6fa90b370baeae907
yohanlb <yohan.lbtn@proton.me>
2024-04-22 13:50:15 +0200

    fix: update gameDeck.json path

--- src/queries/gameDecks.ts
+++ src/queries/gameDecks.ts
@@ -27,7 +27,7 @@ export async function getGameDeckByName(name: string) {
 
 export async function getGameDecksFromJson() {
   const file = await fs.readFile(
-    process.cwd() + '/src/data/gameDecks.json',
+    process.cwd() + '/src/lib/data/gameDecks.json',
     'utf8',
   );
   const data = JSON.parse(file);
