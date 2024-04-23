88569d1171eb98aa446bc7848a72fbd6b2d8aad2
yohanlb <yohan.lbtn@proton.me>
2024-04-23 19:10:40 +0200

    fix: fix style gameDeckCard

--- src/components/home/GameDeckCard.tsx
+++ src/components/home/GameDeckCard.tsx
@@ -13,15 +13,15 @@ const GameDeckCard = ({ gameDeck }: Props) => {
   const dynamicImageName = gameDeck.name as GameDeck['name'];
   const image = gameDeckImages[dynamicImageName];
 
-  if (!image) {
-    return (
-      <div className='group flex aspect-[16/9] w-full flex-col justify-between rounded-lg bg-background p-1 text-left text-sm hover:bg-zinc-700'>
+  return (
+    <div className='group relative aspect-[16/10] w-full justify-between overflow-hidden rounded-lg bg-background p-1 text-left text-sm hover:bg-zinc-700 sm:p-2'>
+      <div className='relative z-10 flex h-full flex-col '>
         <div className='flex justify-between'>
           <ChipScore label='87%' />
           <ChipCommunity label='54%' />
         </div>
         <div className='flex grow items-center justify-center text-center'>
-          <h3 className='text-base font-bold leading-none tracking-wider text-white underline-offset-4 group-hover:underline'>
+          <h3 className='line-clamp-2 text-base font-bold leading-none tracking-wide text-white underline-offset-4 group-hover:underline sm:text-xl'>
             {gameDeck.displayName}
           </h3>
         </div>
@@ -32,39 +32,21 @@ const GameDeckCard = ({ gameDeck }: Props) => {
           </div>
         </div>
       </div>
-    );
-  } else {
-    return (
-      <div className='group relative flex aspect-[16/9] w-full flex-col justify-between overflow-hidden rounded-lg bg-black/20 p-1 text-left text-sm transition-all duration-200 hover:bg-black/0 '>
-        <div className='flex justify-between'>
-          <ChipScore label='87%' />
-          <ChipCommunity label='54%' />
-        </div>
-        <div className='flex grow items-center justify-center text-center'>
-          <h3 className='text-base font-bold leading-none tracking-wider text-white underline-offset-4 group-hover:underline'>
-            {gameDeck.displayName}
-          </h3>
-        </div>
-        <div className='flex justify-end'>
-          <div className='flex gap-1'>
-            <span className='text-xs'>{gameDeck.countryIds.length}</span>
-            <IconDeck />
-          </div>
-        </div>
+      {image && (
         <Image
           alt={gameDeck.displayName}
           src={image}
           fill
           sizes={'600px'}
-          className=' duration-300 ease-out group-hover:scale-105'
+          className='opacity-70 duration-300 ease-out group-hover:scale-105 group-hover:opacity-100'
           style={{
             objectFit: 'cover',
-            zIndex: -1,
+            zIndex: 5,
           }}
         />
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 };
 
 export default GameDeckCard;
