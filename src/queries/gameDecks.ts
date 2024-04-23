69dbf9e18149c8f9a15792e4b3240eab3abf6a43
yohanlb <yohan.lbtn@proton.me>
2024-04-23 14:25:08 +0200

    fix: attempt to fix json import

--- src/queries/gameDecks.ts
+++ src/queries/gameDecks.ts
@@ -1,4 +1,5 @@
 import { promises as fs } from 'fs';
+import path from 'path';
 
 const ONE_HOUR = 60 * 60;
 const baseUrl = process.env.GEOQUIZ_API_BASE_URL as string;
@@ -28,7 +29,7 @@ export async function getGameDeckByName(name: string) {
 
 export async function getGameDecksFromJson() {
   const file = await fs.readFile(
-    process.cwd() + '/src/lib/data/gameDecks.json',
+    path.join(process.cwd(), '/src/lib/data/gameDecks.json'),
     'utf8',
   );
   const data = JSON.parse(file) as GameDeck[];
@@ -43,7 +44,7 @@ export async function getGameDecksFromJsonByName(
   gameDeckName: GameDeck['name'],
 ) {
   const file = await fs.readFile(
-    process.cwd() + '/src/lib/data/gameDecks.json',
+    path.join(process.cwd(), '/src/lib/data/gameDecks.json'),
     'utf8',
   );
   const data = JSON.parse(file) as GameDeck[];
