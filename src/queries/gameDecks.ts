86920f8914783330893fb69ffaa03704e15e953b
yohanlb <yohan.lbtn@proton.me>
2024-04-24 18:18:00 +0200

    feat: update game deck url

--- src/queries/gameDecks.ts
+++ src/queries/gameDecks.ts
@@ -5,7 +5,7 @@ const ONE_HOUR = 60 * 60;
 const baseUrl = process.env.NEXT_PUBLIC_GEOQUIZ_API_BASE_URL as string;
 
 export async function getDeckByName(deckName: string) {
-  const response = await fetch(`${baseUrl}/gameDecks/name/${deckName}`, {
+  const response = await fetch(`${baseUrl}/decks/${deckName}`, {
     next: { revalidate: ONE_HOUR },
   });
   if (!response.ok) {
