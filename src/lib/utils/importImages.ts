514006fd69582a0797f9918c0e06b50c5cab08d7
Yohan <yohan.lbtn@proton.me>
2024-05-15 14:57:26 +0200

    feat: update game decks images

--- src/lib/utils/importImages.ts
+++ src/lib/utils/importImages.ts
@@ -1,12 +1,12 @@
-import africa from '@assets/gameDeckImages/africa.jpg';
-import europe from '@assets/gameDeckImages/europe.jpg';
-import asia from '@assets/gameDeckImages/asia.jpg';
-import islands from '@assets/gameDeckImages/islands.jpg';
-import southAmerica from '@assets/gameDeckImages/southAmerica.jpg';
-import worldwide from '@assets/gameDeckImages/worldwide.jpg';
-import caribbean from '@assets/gameDeckImages/caribbean.jpg';
-import oceania from '@assets/gameDeckImages/oceania.jpg';
-import northernAndCentralAmerica from '@assets/gameDeckImages/northernAndCentralAmerica.jpg';
+import africa from '@assets/gameDeckImages/Africa.jpg';
+import europe from '@assets/gameDeckImages/Europe.jpg';
+import asia from '@assets/gameDeckImages/Asia.jpg';
+import islands from '@assets/gameDeckImages/Islands.jpg';
+import southAmerica from '@assets/gameDeckImages/SouthAmerica.jpg';
+import worldwide from '@assets/gameDeckImages/Worldwide.jpg';
+import caribbean from '@assets/gameDeckImages/Caribbean.jpg';
+import oceania from '@assets/gameDeckImages/Oceania.jpg';
+import northernAndCentralAmerica from '@assets/gameDeckImages/NorthAmerica.jpg';
 import { StaticImageData } from 'next/image';
 
 type GameDeckImages = {
