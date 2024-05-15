b4270d2e65fce789cb6d101eb9468a682f7ae504
Yohan <yohan.lbtn@proton.me>
2024-05-15 20:22:12 +0200

    fix: update dialog padding

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -36,7 +36,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid max-h-[95%] w-full max-w-xs translate-x-[-50%] translate-y-[-50%] gap-4 overflow-y-auto rounded-lg bg-white p-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] dark:bg-background/95 sm:max-w-lg sm:rounded-xl ',
+        'fixed left-[50%] top-[50%] z-50 grid max-h-[95%] w-full max-w-xs translate-x-[-50%] translate-y-[-50%] gap-4 overflow-y-auto rounded-lg bg-white p-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] dark:bg-background/95 sm:max-w-md sm:rounded-xl sm:p-8 ',
         className,
       )}
       {...props}
