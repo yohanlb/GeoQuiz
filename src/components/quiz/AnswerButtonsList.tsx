5250df7cdeaa95c9838a3b1be6b005fead7c7273
Yohan <yohan.lbtn@proton.me>
2024-05-15 14:11:28 +0200

    feat: add hotkeys to answer questions

--- src/components/quiz/AnswerButtonsList.tsx
+++ src/components/quiz/AnswerButtonsList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import AnswerButton, { AnswerOptionButton } from './AnswerButton';
+import { useHotkeys } from 'react-hotkeys-hook';
 
 type AnswerButtonsListProps = {
   options: AnswerOptionButton[];
@@ -7,6 +8,11 @@ type AnswerButtonsListProps = {
 };
 
 function AnswerButtonsList({ options, handleClick }: AnswerButtonsListProps) {
+  useHotkeys('1', () => handleClick(options[0].text), [options]);
+  useHotkeys('2', () => handleClick(options[1].text), [options]);
+  useHotkeys('3', () => handleClick(options[2].text), [options]);
+  useHotkeys('4', () => handleClick(options[3].text), [options]);
+
   return (
     <ul className='items- flex flex-col justify-between gap-2 py-2 md:flex-row md:flex-wrap md:gap-4'>
       {options.map((option, index) => (
