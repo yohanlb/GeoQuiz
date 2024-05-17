3e9b8adba3e9e923fe0c8baca84249d7c52bdcd0
Yohan <yohan.lbtn@proton.me>
2024-05-18 01:50:11 +0200

    feat: implement feature section

--- src/components/home/FeaturedAndPopularSection.tsx
+++ src/components/home/FeaturedAndPopularSection.tsx
@@ -8,10 +8,18 @@ type Props = {
 const FeaturedAndPopularSection = ({ decks }: Props) => {
   const howManyToDisplay = 6;
 
+  const featuredAndPopularDecks = decks.filter(
+    (deck) =>
+      deck.categories?.includes('featured') ||
+      deck.categories?.includes('popular'),
+  );
+
   return (
     <section>
       <h2>Featured and Popular 🔥</h2>
-      <GameDeckGrid decks={decks.slice(0, howManyToDisplay)} />
+      <GameDeckGrid
+        decks={featuredAndPopularDecks.slice(0, howManyToDisplay)}
+      />
     </section>
   );
 };
