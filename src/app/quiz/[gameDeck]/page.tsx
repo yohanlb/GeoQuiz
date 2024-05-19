671b864f17b84e12a7193b6c3597fd170ee07c56
Yohan <yohan.lbtn@proton.me>
2024-05-19 23:44:37 +0200

    feat: prepare game wrapper for dynamic decks

--- src/app/quiz/[gameDeck]/page.tsx
+++ src/app/quiz/[gameDeck]/page.tsx
@@ -3,11 +3,19 @@ import GameClientWrapper from '@components/quiz/GameClientWrapper';
 
 type Props = {
   params: { gameDeck: string };
-  searchParams: { length: number };
+  searchParams: { length: number; dynamicCountryIds: string };
 };
 const Quiz = async ({ params, searchParams }: Props) => {
   const deck = await getDeckByName(params.gameDeck);
 
+  if (deck.isDynamic && searchParams.dynamicCountryIds) {
+    // to implement dynamic deck, provide it to search params: ?dynamicCountryIds=75,1
+    const deckIds = searchParams.dynamicCountryIds
+      .split(',')
+      .map(Number) as CountryData['id'][];
+    deck.countryIds = deckIds;
+    deck.isDynamic = true;
+  }
   return (
     <div className='h-full'>
       <GameClientWrapper deck={deck} amountOfQuestions={searchParams.length} />
