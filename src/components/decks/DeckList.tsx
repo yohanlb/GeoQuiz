0ea1759b168e2227c0d9a04139162cdd3ead7834
Yohan <yohan.lbtn@proton.me>
2024-05-20 17:17:31 +0200

    refactor: factorize DeckItem

--- src/components/decks/DeckList.tsx
+++ src/components/decks/DeckList.tsx
@@ -1,38 +1,18 @@
-import Link from 'next/link';
 import React from 'react';
+import DeckItem from './DeckItem';
 
 type DeckListProps = {
   decks: Deck[];
 };
 
-const tempPlayerScore = null;
-
 const DeckList: React.FC<DeckListProps> = ({ decks }) => {
   return (
     <ul className='flex flex-col space-y-2 bg-[#1C2027]'>
       {decks.map((deck) => (
-        <li key={deck.id}>
-          <Link href={`./quiz/${deck.name}`} className='block  '>
-            <div className='flex items-center justify-between rounded-md border-[0.5px] border-gray-700 px-2 py-1 shadow-md shadow-gray-950 transition hover:border-gray-500 active:bg-gray-300 active:text-black'>
-              <h2 className='max-w-52 truncate text-sm font-light'>
-                {deck.displayName}
-              </h2>
-              <div className='flex items-center space-x-1 font-mono'>
-                <span className=' text-right font-mono'>
-                  {tempPlayerScore !== null ? `🏅${tempPlayerScore}` : '📦'}
-                </span>
-                <span className=' text-right '>🌍{deck.countryIds.length}</span>
-                <span className=' text-right'>
-                  📈{Math.round(deck.averageSuccessRatio)}%
-                </span>
-              </div>
-            </div>
-          </Link>
-        </li>
+        <DeckItem key={deck.id} deck={deck} />
       ))}
     </ul>
   );
 };
 
 export default DeckList;
-// hover:bg-blue-100
