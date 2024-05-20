7cbfe5545bfdde05c34fb7c3de58b9583b8e3aac
Yohan <yohan.lbtn@proton.me>
2024-05-20 18:18:36 +0200

    feat: add link to deck page on homepage

--- src/app/decks/page.tsx
+++ src/app/decks/page.tsx
@@ -5,13 +5,17 @@ import React from 'react';
 
 async function page() {
   const decks = await getDecks();
+  decks.sort((a, b) => a.name.localeCompare(b.name));
 
   return (
     <div className='mx-auto max-w-md px-2 md:px-0'>
       <h2 className='my-2 text-base tracking-wide'>All Decks</h2>
       <DeckList decks={decks} />
       <div className='mt-4 w-full text-center'>
-        <Link href='/' className='text-blue-500 hover:underline'>
+        <Link
+          href='/'
+          className='underline underline-offset-4 hover:text-blue-500'
+        >
           Back
         </Link>
       </div>
