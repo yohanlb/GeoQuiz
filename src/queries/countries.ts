5e95aa3aac0533d081183b093ecffd7d5e3a9319
Yohan <yohan.lbtn@proton.me>
2024-05-21 20:28:24 +0200

    feat: update fetch countries query

--- src/queries/countries.ts
+++ src/queries/countries.ts
@@ -1,5 +1,5 @@
 const ONE_HOUR = 60 * 60;
-const baseUrl = process.env.GEOQUIZ_API_BASE_URL as string;
+const baseUrl = process.env.NEXT_PUBLIC_GEOQUIZ_API_BASE_URL as string;
 
 export async function getCountryByCode(countryCode: string) {
   const response = await fetch(`${baseUrl}/countries/iso3/${countryCode}`, {
@@ -14,24 +14,37 @@ export async function getCountryByCode(countryCode: string) {
   return data;
 }
 
-export async function getAllCountries() {
+async function fetchCountries(): Promise<CountryData[]> {
   const response = await fetch(`${baseUrl}/countries/`, {
     next: { revalidate: ONE_HOUR * 24 },
   });
   if (!response.ok) {
     throw new Error('Countries not found');
   }
+  const { data }: { data: CountryData[] } = await response.json();
+  return data;
+}
 
-  const { data: allCountries }: { data: CountryData[] } = await response.json();
+export async function getAllCountries(): Promise<CountryData[]> {
+  return await fetchCountries();
+}
 
-  //   const groupedCountries = allCountries.reduce((acc: any, country) => {
-  //     const subregion = country.subregion;
-  //     if (!acc[subregion]) {
-  //       acc[subregion] = [];
-  //     }
-  //     acc[subregion].push(country);
-  //     return acc;
-  //   }, {});
+export async function getAllCountriesGrouped(): Promise<{
+  [key: string]: CountryData[];
+}> {
+  const allCountries = await fetchCountries();
 
-  return allCountries;
+  const groupedCountries = allCountries.reduce(
+    (acc: { [key: string]: CountryData[] }, country) => {
+      const subregion = country.subregion;
+      if (!acc[subregion]) {
+        acc[subregion] = [];
+      }
+      acc[subregion].push(country);
+      return acc;
+    },
+    {},
+  );
+
+  return groupedCountries;
 }
