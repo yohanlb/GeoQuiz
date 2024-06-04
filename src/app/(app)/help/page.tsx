b2363d75a4bffb8a390c75c3c7062c7d8c72a50f
Yohan <yohan.lbtn@proton.me>
2024-06-04 19:34:30 +0200

    feat: imrpove help page

--- src/app/(app)/help/page.tsx
+++ src/app/(app)/help/page.tsx
@@ -3,14 +3,21 @@ import { navigationLinks } from '@lib/navigationLinks';
 import Link from 'next/link';
 import PageCenteredLink from '@components/_commons/PageCenteredLink';
 import SectionTitle from '@components/_commons/SectionTitle';
+import HowItWorksSection from '@components/help/HowItWorksSection';
 import { Button } from '@components/ui/button';
 
 const Help = async () => {
   return (
-    <div className='flex flex-col gap-6 px-4 py-2 md:px-0'>
-      <SectionTitle text='Help' />
-      <section className='text-center flex flex-col gap-4 items-center'>
-        <SectionTitle text='Any issue, feedback or suggestion ?' variant='h3' />
+    <div className='flex flex-col gap-6 px-4 py-2 md:px-0 max-w-md mx-auto'>
+      {/* <SectionTitle text='Help' /> */}
+      <HowItWorksSection />
+      <hr />
+      <section className='flex flex-col gap-4 '>
+        <SectionTitle text='Feedbacks' variant='h3' />
+        <SectionTitle
+          text='Any issue, feedback or suggestion ?'
+          variant='description'
+        />
         <Button variant={'outline'} className='w-fit'>
           <Link
             href={'https://forms.gle/YrScov3rJU7dEdWS8'}
