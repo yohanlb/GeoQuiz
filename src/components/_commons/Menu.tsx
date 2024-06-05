b5b451cb2712f8d73482453492a19302eaf9046e
Yohan <yohan.lbtn@proton.me>
2024-06-05 23:45:40 +0200

    fix: fix menu link sizes

--- src/components/_commons/Menu.tsx
+++ src/components/_commons/Menu.tsx
@@ -19,38 +19,38 @@ function Menu({ trigger }: Props) {
       <DropdownMenuTrigger asChild>{trigger}</DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
         <DropdownMenuItem>
-          <Link href={navigationLinks.home.href}>
+          <Link href={navigationLinks.home.href} className='w-full'>
             {navigationLinks.home.label}
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Link href={navigationLinks.allDecks.href}>
+          <Link href={navigationLinks.allDecks.href} className='w-full'>
             {navigationLinks.allDecks.label}
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Link href={navigationLinks.countries.href}>
+          <Link href={navigationLinks.countries.href} className='w-full'>
             {navigationLinks.countries.label}
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Link href={navigationLinks.history.href}>
+          <Link href={navigationLinks.history.href} className='w-full'>
             {navigationLinks.history.label}
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem disabled>
-          <Link href={navigationLinks.stats.href}>
+          <Link href={navigationLinks.stats.href} className='w-full'>
             {navigationLinks.stats.label}
           </Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem disabled>
-          <Link href={navigationLinks.more.href}>
+          <Link href={navigationLinks.more.href} className='w-full'>
             {navigationLinks.more.label}
           </Link>
         </DropdownMenuItem>
         <DropdownMenuItem>
-          <Link href={navigationLinks.help.href}>
+          <Link href={navigationLinks.help.href} className='w-full'>
             {navigationLinks.help.label}
           </Link>
         </DropdownMenuItem>
