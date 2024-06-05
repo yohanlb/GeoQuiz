df0375f984357ef41ea6f2299b54207040c2955a
Yohan <yohan.lbtn@proton.me>
2024-06-05 16:09:07 +0200

    feat: instal nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { nextui } from '@nextui-org/theme';
 import type { Config } from 'tailwindcss';
 
 const config = {
@@ -6,6 +7,8 @@ const config = {
     './src/components/**/*.{ts,tsx}',
     './src/app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+
   ],
   darkMode: ['class'],
   prefix: '',
@@ -53,7 +56,7 @@ const config = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [require('tailwindcss-animate'), nextui()],
 } satisfies Config;
 
 export default config;
