9ba4e81250f93409fbb4a296fbb429a4c375ae45
Yohan <yohan.lbtn@proton.me>
2024-06-05 23:19:36 +0200

    refactor: clean test files

--- src/__tests__/pages/landing.test.js
+++ src/__tests__/pages/landing.test.js
@@ -3,6 +3,11 @@ import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
 describe('Landing Page', () => {
+  it('renders homepage unchanged', () => {
+    const { container } = render(<Landing />);
+    expect(container).toMatchSnapshot();
+  });
+
   it('renders a heading', () => {
     render(<Landing />);
     const heading = screen.getByRole('heading', { level: 1 });
