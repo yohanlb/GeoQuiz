592dba3e21ecdcfbdaa7cfa29e6bc7a35442827a
Yohan <yohan.lbtn@proton.me>
2024-06-10 15:58:12 +0200

    feat: rework last attempt part

--- src/components/quiz/LastAttempts.tsx
+++ src/components/quiz/LastAttempts.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 
 const LastAttempts = ({ results }: { results: boolean[] }) => {
+  const lastResults = [...results].slice(-5);
+
   return (
-    <div className='flex items-center space-x-1'>
-      {results
-        .slice(-10)
-        .reverse()
-        .map((result, index) => (
-          <span
-            key={index}
-            className={`${result ? 'text-green-500' : 'text-red-500'}`}
-          >
-            {result ? '✔' : '✘'}
-          </span>
-        ))}
+    <div className='flex flex-row-reverse items-center gap-1'>
+      {[...Array(5 - lastResults.length)].map((_, index) => (
+        // show placeholder if not enough results.
+        <span key={index} className='space-x-3 font-mono text-sm'>
+          _
+        </span>
+      ))}
+      {lastResults.map((result, index) => (
+        <span
+          key={index}
+          className={`font-mono text-sm ${result ? 'text-green-500' : 'text-red-500'}`}
+        >
+          {result ? '✔' : '✘'}
+        </span>
+      ))}
     </div>
   );
 };
