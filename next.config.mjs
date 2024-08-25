cdf855e48827337199f91b07079cc80002295c3b
yohanlb <yohan.lbtn@proton.me>
2024-08-25 16:25:54 +0200

    chores: setup sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withSentryConfig } from '@sentry/nextjs';
+
 /** @type {import('next').NextConfig} */
 import withPWAInit from 'next-pwa';
 
@@ -6,37 +8,74 @@ const withPWA = withPWAInit({
   disable: process.env.NODE_ENV === 'development',
 });
 
-export default withPWA({
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'adwhviervvdqnbswunen.supabase.co',
-        port: '',
-        pathname: '/storage/**',
-      },
-    ],
-  },
-  logging: {
-    fetches: {
-      fullUrl: true,
+export default withSentryConfig(
+  withPWA({
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'adwhviervvdqnbswunen.supabase.co',
+          port: '',
+          pathname: '/storage/**',
+        },
+      ],
     },
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/ingest/static/:path*',
-        destination: 'https://eu-assets.i.posthog.com/static/:path*',
+    logging: {
+      fetches: {
+        fullUrl: true,
       },
-      {
-        source: '/ingest/:path*',
-        destination: 'https://eu.i.posthog.com/:path*',
-      },
-    ];
-  },
-  skipTrailingSlashRedirect: true,
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/ingest/static/:path*',
+          destination: 'https://eu-assets.i.posthog.com/static/:path*',
+        },
+        {
+          source: '/ingest/:path*',
+          destination: 'https://eu.i.posthog.com/:path*',
+        },
+      ];
+    },
+    skipTrailingSlashRedirect: true,
+  }),
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
-});
+    org: 'yohanlb',
+    project: 'geoquiz-nextjs',
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Automatically annotate React components to show their full name in breadcrumbs and session replay
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: '/monitoring',
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true,
+  },
+);
