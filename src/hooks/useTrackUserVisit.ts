ad1b3c6d82b0cfb44b363808a6d7a9577b550394
yohanlb <yohan.lbtn@proton.me>
2024-08-28 22:44:06 +0200

    fix: fix PH event date format

--- src/hooks/useTrackUserVisit.ts
+++ src/hooks/useTrackUserVisit.ts
@@ -7,30 +7,35 @@ export default function useTrackUserVisit() {
   const posthog = usePostHog();
 
   useEffect(() => {
-    const now = new Date().getTime();
-    const visitThreshold = 4 * 60 * 60 * 1000; // 4 hours in milliseconds
+    const now = new Date();
+    const visitThresholdDuration = 4 * 60 * 60 * 1000; // 4 hours in milliseconds
 
     let visitCount = parseInt(localStorage.getItem('u_vc') || '0');
-    const lastVisit = localStorage.getItem('u_lv');
-    let firstVisit = localStorage.getItem('u_fv');
 
-    // If firstVisit doesn't exist, set it
-    if (!firstVisit) {
-      firstVisit = now.toString();
-      localStorage.setItem('u_fv', firstVisit);
+    const lastVisitDateStr = localStorage.getItem('u_lv');
+    const lastVisitDate = lastVisitDateStr ? new Date(lastVisitDateStr) : null;
+    const firstVisitDateStr = localStorage.getItem('u_fv');
+    let firstVisitDate = firstVisitDateStr ? new Date(firstVisitDateStr) : null;
+    if (!firstVisitDate) {
+      firstVisitDate = now;
+      localStorage.setItem('u_fv', firstVisitDate.toISOString());
     }
 
+    const isPastThreshold =
+      !lastVisitDate ||
+      now.getTime() - lastVisitDate.getTime() > visitThresholdDuration;
+
     // If lastVisit doesn't exist or the threshold has passed, increment the visit count
-    if (!lastVisit || now - parseInt(lastVisit) > visitThreshold) {
+    if (isPastThreshold) {
       visitCount += 1;
       localStorage.setItem('u_vc', visitCount.toString());
-      localStorage.setItem('u_lv', now.toString());
+      localStorage.setItem('u_lv', now.toISOString());
     }
 
     const visitData = {
       visitCount,
-      firstVisit: new Date(parseInt(firstVisit)).toISOString(),
-      lastVisit: lastVisit ? new Date(parseInt(lastVisit)).toISOString() : null,
+      firstVisit: firstVisitDate,
+      lastVisit: lastVisitDate,
     };
 
     const isNewUser = visitCount <= 1;
@@ -38,15 +43,19 @@ export default function useTrackUserVisit() {
     if (posthog) {
       posthog.people.set({
         visit_count: visitData.visitCount,
-        first_visit: visitData.firstVisit,
-        last_visit: visitData.lastVisit,
+        first_visit: visitData.firstVisit.toISOString(),
+        last_visit: visitData.lastVisit
+          ? visitData.lastVisit.toISOString()
+          : null,
         is_new_user: isNewUser,
       });
 
       posthog.capture('user_visit', {
         visit_count: visitData.visitCount,
-        first_visit: visitData.firstVisit,
-        last_visit: visitData.lastVisit,
+        first_visit: visitData.firstVisit.toISOString(),
+        last_visit: visitData.lastVisit
+          ? visitData.lastVisit.toISOString()
+          : null,
         is_new_user: isNewUser,
       });
     }
