01d3be8d7c9ec1d4f28fa7fbde675709aa107667
yohanlb <yohan.lbtn@proton.me>
2024-09-07 01:09:47 +0200

    feat: add twitter link in footer

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { navigationLinks } from '@lib/navigationLinks';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import TwitterButton from '@components/_commons/TwitterButton';
 
 const Footer = () => {
   const currentPath = usePathname();
@@ -17,34 +18,37 @@ const Footer = () => {
   return (
     <div className='mt-4 flex flex-col items-center justify-center gap-4 py-4 text-sm text-gray-400 md:mt-16'>
       <hr className='w-4/5 border-gray-700' />
-      <p>
-        Entirely brought to you by{' - '}
-        <Link
-          href='https://yohanlebreton.com'
-          target='_blank'
-          className='font-bold italic tracking-wider underline underline-offset-2'
-        >
-          Yohan LB!
-        </Link>
-      </p>
-      <p>
-        <Link
-          href={'https://forms.gle/YrScov3rJU7dEdWS8'}
-          target='_blank'
-          rel='noreferrer'
-          className='underline underline-offset-2'
-        >
-          Contact
-        </Link>
-        {' - '}
-        <Link
-          href={'https://forms.gle/YrScov3rJU7dEdWS8'}
-          target='_blank'
-          rel='noreferrer'
-          className='underline underline-offset-2'
-        >
-          Feedbacks
-        </Link>
+      <TwitterButton />
+      <p className='flex gap-4'>
+        <span>
+          Entirely brought to you by{' - '}
+          <Link
+            href='https://yohanlebreton.com'
+            target='_blank'
+            className='font-bold italic tracking-wider underline underline-offset-2'
+          >
+            Yohan LB!
+          </Link>
+        </span>
+        <span>
+          <Link
+            href={'https://forms.gle/YrScov3rJU7dEdWS8'}
+            target='_blank'
+            rel='noreferrer'
+            className='underline underline-offset-2'
+          >
+            Contact
+          </Link>
+          {' - '}
+          <Link
+            href={'https://forms.gle/YrScov3rJU7dEdWS8'}
+            target='_blank'
+            rel='noreferrer'
+            className='underline underline-offset-2'
+          >
+            Feedbacks
+          </Link>
+        </span>
       </p>
     </div>
   );
