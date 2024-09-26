823f1a3a5b079d7f6e376e8a3f7b2a6c71353473
yohanlb <yohan.lbtn@proton.me>
2024-09-26 19:56:11 +0200

    feat: add dedicated login page

---
+++ src/app/(app)/login/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { navigationLinks } from '@lib/navigationLinks';
+import { createClient } from '@lib/supabase/server';
+import { redirect } from 'next/navigation';
+import LoginButton from '@components/login/LoginButton';
+
+async function LoginPage() {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect(navigationLinks.home.href);
+  }
+  return (
+    <div className='mt-12'>
+      <p className='my-4 text-center text-sm text-gray-400'>
+        Please note that the authentication feature is still in development.
+        <br />
+        Some functionalities may not work as expected.
+      </p>
+      <div className='mx-auto flex max-w-sm flex-col gap-4 rounded-lg bg-background p-4 text-white'>
+        <h1 className='text-2xl font-bold'>Sign In</h1>
+        <h2>Sign in to your account using Google OAuth.</h2>
+        <div className='flex items-center justify-center'>
+          <LoginButton />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
