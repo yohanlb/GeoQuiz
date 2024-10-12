ef64af98eb9ce05461448d07f601cf759f5e0ae5
yohanlb <yohan.lbtn@proton.me>
2024-10-12 18:09:45 +0200

    fix: fix guess list order

--- src/components/_commons/GuessesList.tsx
+++ src/components/_commons/GuessesList.tsx
@@ -35,16 +35,16 @@ const GuessesList = ({
   return (
     <div className='inline-flex gap-2'>
       <div className='flex items-center gap-1'>
-        {/* Show placeholder if not enough history results. */}
-        {[...Array(extraEmptySquaresToShow)].map((_, index) => (
-          <AttemptSquare key={index} status='default' />
-        ))}
         {/* Show history results */}
         {historyReversed.map((result, index) => (
           <AttemptSquare key={index} status={result ? 'valid' : 'invalid'} />
         ))}
         {/* Show an empty square until user click on an answer */}
         {showNewResult && <AttemptSquare status={newResult} isLast={true} />}
+        {/* Show placeholder if not enough history results. */}
+        {[...Array(extraEmptySquaresToShow)].map((_, index) => (
+          <AttemptSquare key={index} status='default' />
+        ))}
       </div>
       <CountryScoreBadge countryScoreStatus={countryScoreStatus} />
     </div>
