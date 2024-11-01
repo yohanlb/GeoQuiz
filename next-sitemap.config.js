52920f559b9851a0f281edad96c823fb4aa5ff78
yohanlb <yohan.lbtn@proton.me>
2024-11-01 20:01:05 +0100

    feat: configure dynamic sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,31 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://geoquiz.co',
   generateRobotsTxt: true,
-  exclude: ['/user'],
+  exclude: ['/user', '/quiz', '/daily/test'],
+  transform: async (config, path) => {
+    // Priority 1
+    if (path === '/' || path === '/home' || path === '/daily') {
+      return {
+        loc: path,
+        changefreq: 'daily',
+        priority: 1.0,
+      };
+    }
+
+    //Priority 0.9
+    if (path === '/countries' || path === '/decks') {
+      return {
+        loc: path,
+        changefreq: 'daily',
+        priority: 0.8,
+      };
+    }
+
+    // Default priority for all other pages
+    return {
+      loc: path,
+      changefreq: 'monthly',
+      priority: 0.8,
+    };
+  },
 };
