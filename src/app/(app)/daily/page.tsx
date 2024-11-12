dafd22eeadd25dc510022a720872c331f170299b
yohanlb <yohan.lbtn@proton.me>
2024-11-12 18:36:22 +0100

    feat: create new daily page and rework cotd

--- src/app/(app)/daily/page.tsx
+++ src/app/(app)/daily/page.tsx
@@ -1,20 +1,35 @@
 import React from 'react';
-import CotdPageContent from '@features/daily/components/CotdPageContent';
+import { navigationLinks } from '@lib/data/navigation-links';
+import Image from 'next/image';
+import Link from 'next/link';
+import MysteryCountryIllustration from '@assets/mystery-country-illustration.png';
+import SectionTitle from '@components/global/SectionTitle';
 
-export const metadata = {
-  title: 'Country of the Day',
-  description: 'Play the country of the day quiz.',
-};
-
-async function CountryOfTheDayPage() {
+function Page() {
   return (
-    <div className='mx-auto flex max-w-md flex-col gap-6 px-4 py-2 md:px-0'>
-      <header>
-        <h1 className='font text-left text-xl'>Country of the Day</h1>
-      </header>
-      <CotdPageContent />
+    <div className='flex flex-col gap-6 p-4 md:p-0'>
+      <SectionTitle variant='h2' centered text='Daily Challenges 🌎' />
+      <Link
+        href={navigationLinks.mysteryCountry.href}
+        className='block w-full transition-opacity hover:opacity-90'
+      >
+        <div className='transition-bg flex w-full items-center gap-4 overflow-hidden rounded-lg border border-gray-600 bg-background p-2 hover:bg-stone-900/20 md:gap-6 md:p-4'>
+          <div className='relative h-16 w-16 flex-shrink-0 overflow-hidden rounded-md md:h-24 md:w-24'>
+            <Image
+              src={MysteryCountryIllustration}
+              alt='Mystery Country Illustration'
+            />
+          </div>
+          <div className='flex flex-grow flex-col justify-center'>
+            <h2 className='mb-2 text-xl font-bold text-gray-100'>
+              {navigationLinks.mysteryCountry.label}
+            </h2>
+            <p className='text-lg text-gray-400'>A new country every day!</p>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
 
-export default CountryOfTheDayPage;
+export default Page;
--- src/features/welcome/components/home/DailyChallengesSection.tsx
+++ src/features/welcome/components/home/DailyChallengesSection.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { navigationLinks } from '@lib/data/navigation-links';
 import { Card } from '@nextui-org/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import SectionTitle from '@components/global/SectionTitle';
 
-const CountryOfTheDay = () => {
+const DailyChallengesSection = () => {
   return (
     <section>
-      <SectionTitle text='Country of the Day 🌎' />
-      <SectionTitle text='One new country each day!' variant='description' />
+      <SectionTitle text='Daily Challenges 🌎' />
+      <SectionTitle text='New questions every day!' variant='description' />
       <div className='h-48 py-1'>
-        <Link href='/daily'>
+        <Link href={navigationLinks.daily.href}>
           <Card className='group flex h-full w-full items-center justify-center rounded-lg bg-neutral-800'>
             <h3 className='z-10 text-8xl text-white duration-300 ease-in-out group-hover:text-9xl'>
               ?
@@ -32,4 +33,4 @@ const CountryOfTheDay = () => {
   );
 };
 
-export default CountryOfTheDay;
+export default DailyChallengesSection;
