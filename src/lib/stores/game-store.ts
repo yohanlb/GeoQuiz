ef6689985c6ab61ad15926d1bf03d3d1a3016842
yohanlb <yohan.lbtn@proton.me>
2024-12-09 19:15:08 +0100

    chores: add comments to game-store

--- src/lib/stores/game-store.ts
+++ src/lib/stores/game-store.ts
@@ -2,31 +2,40 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 interface GameStoreState {
-  currentQuestionIndex: number;
+  //Game Status
   gameState: GameStatus; //TODO: rename game status
   setGameState: (gameState: GameStatus) => void;
+  isGameStoreInitialized: boolean;
+  setIsGameStoreInitialized: (isGameStoreInitialized: boolean) => void;
+  resetGame: () => void;
+
+  // Game Questions
+  currentQuestionIndex: number;
   incrementQuestionIndex: () => void;
   questionType: QuestionType;
   toggleQuestionType: () => void;
   setQuestionType: (questionType: QuestionType) => void;
   isShowingAnswer: boolean;
-  setIsShowingAnswer: (isShowingAnswer: boolean) => void;
   userAnswers: string[];
+  setIsShowingAnswer: (isShowingAnswer: boolean) => void;
   resetUserAnswers: () => void;
   addToUserAnswers: (userAnswer: string) => void;
+  answeredQuestions: AnsweredQuestion[];
+  addToAnsweredQuestions: (answeredQuestion: AnsweredQuestion) => void;
+
+  // Results
   userCountryResults: UserCountryResult[];
   addToUserCountryResults: (
     countryId: CountryRecord['id'],
     result: UserResultsStatus,
     questionIndex: number,
   ) => void;
+
+  // Deck Management
   deck: DeckRecord | null;
   setDeck: (deck: DeckRecord) => void;
-  resetGame: () => void;
-  isGameStoreInitialized: boolean;
-  setIsGameStoreInitialized: (isGameStoreInitialized: boolean) => void;
-  answeredQuestions: AnsweredQuestion[];
-  addToAnsweredQuestions: (answeredQuestion: AnsweredQuestion) => void;
+
+  // User Scores For current series
   userCountryScoresForCurrentSeries: CountryScoreHistory;
   setUserCountryScoresForCurrentSeries: (
     userCountryScoresForCurrentSeries: CountryScoreHistory,
