7e70154f392f6169fdd33ba3bbc22545ef8d0698
yohanlb <yohan.lbtn@proton.me>
2024-12-14 21:19:46 +0100

    refactor: improve layout and accessibility for country capital page, add metadata and country flags

--- src/app/(app)/resources/what-is-capital-of/page.tsx
+++ src/app/(app)/resources/what-is-capital-of/page.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import ReactCountryFlag from 'react-country-flag';
 import { getAllCountries } from '@features/countries/server/db/countries';
 import { navigationLinks } from '@lib/data/navigation-links';
 import Link from 'next/link';
+
+export const metadata = {
+  title: 'What is the capital of...',
+  description: 'Learn about capitals of different countries.',
+};
 
 async function page() {
   const countries = await getAllCountries();
 
   return (
-    <div>
-      <h1>What is the capital of...</h1>
-      <ul className=''>
+    <div className='flex flex-col items-center gap-4'>
+      <h1 className='text-3xl'>What is the capital of...</h1>
+      <ul className='grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4'>
         {countries.map((country) => (
-          <li key={country.id} className='text-blue-500 underline'>
+          <li key={country.id} className='flex items-center gap-2'>
+            <ReactCountryFlag
+              className='inline-block'
+              countryCode={country.iso2}
+              svg
+              aria-label={country.name}
+              style={{
+                width: '20px',
+                height: 'auto',
+              }}
+            />
             <Link
+              className='text-blue-500 underline'
               href={`${navigationLinks.resources.href}/what-is-capital-of/${country.id}`}
             >
               {country.name}
